aa9ed20536c291498be050db39dfbdd7e5155503
VitaliyStrunin <sthappy553@gmail.com>
2023-04-24 23:15:04 +0500

    init commit

--- script/script.js
+++ script/script.js
@@ -1,3 +1,31 @@
+import bootstrap from "bootstrap";
+
+import $ from "jquery";
+
+import Popper from "popper.js";
+
+var modal = document.getElementById("jq-modal");
+
+var span = document.getElementsByClassName("jq-close")[0];
+
+$("#openModal").click(function () {
+  modal.style.display = "block";
+});
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+$("#openModal").click(function () {
+  $("#jq-modal").css("display", "block");
+});
+
 $(function () {
   var $slider = $(".slider");
   var $backButton = $(".slider-back");
